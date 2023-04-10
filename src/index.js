ab893c8f5d968152c745d0e2303cb613749c9b89
Chirkov Slava <slavachirkov92@gmail.com>
2023-04-10 22:07:58 +0300

    add timer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import { modal } from "./modules/modal"
 import { slider } from "./modules/slider"
 import { serviceModal } from "./modules/sevrvice-modal"
 import { scroll } from "./modules/scroll"
+import { timer } from "./modules/timer"
 
 modal()
 slider('.benefits-inner', '.benefits__arrow--right', '.benefits__arrow--left', 3, 20)
 slider('.services-inner', '.services__arrow--right', '.services__arrow--left', 2, 0)
 serviceModal()
 scroll()
+timer('16 april 2023')
---
+++ src/modules/timer.js
@@ -0,0 +1,45 @@
+export const timer = (deadline) => {
+    const timerDay = document.querySelector('.count_1 > span'),
+        timerHours = document.querySelector('.count_2 > span'),
+        timerMinutes = document.querySelector('.count_3 > span'),
+        timerSeconds = document.querySelector('.count_4 > span');
+
+    const getTimeRemaining = () => {
+        let dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            days = Math.floor(timeRemaining / 60 / 60 / 24),
+            hours = Math.floor((timeRemaining / 60 / 60) % 24),
+            minuts = Math.floor((timeRemaining / 60) % 60),
+            seconds = Math.floor(timeRemaining % 60);
+
+        return { timeRemaining, days, hours, minuts, seconds }
+    }
+
+    const editTime = num => (num < 10) ? `0${num}` : num
+
+    const updateClock = () => {
+        let getTime = getTimeRemaining()
+
+        timerDay.textContent = editTime(getTime.days)
+        timerHours.textContent = editTime(getTime.hours)
+        timerMinutes.textContent = editTime(getTime.minuts)
+        timerSeconds.textContent = editTime(getTime.seconds)
+    }
+
+    const startTimer = () => {
+        let idTimer = setInterval(() => {
+            let getTime = getTimeRemaining()
+
+            if (getTime.timeRemaining > 0) {
+                updateClock();
+
+                if (getTime.timeRemaining == 0) {
+                    clearTimeout(idTimer);
+                }
+            }
+        }, 1000);
+    }
+
+    startTimer();
+}
