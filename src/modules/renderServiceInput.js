4066e76fcd7f4d5020a4d9d30f8b90ab660f3981
Chirkov Slava <slavachirkov92@gmail.com>
2023-04-13 00:05:10 +0300

    delete error

--- src/modules/renderServiceInput.js
+++ src/modules/renderServiceInput.js
@@ -1,11 +1,13 @@
 export const renderServiceInput = (formId) => {
-    const serviceBtn = document.querySelectorAll('.service-button > a'),
-        form = document.getElementById(formId);
-
-    if (!form) return
+    const serviceBtn = document.querySelectorAll('.service-button > a');
 
     serviceBtn.forEach(btn => {
         btn.addEventListener('click', (e) => {
+            const form = document.getElementById(formId);
+
+            console.log(btn);
+            if (!form) return
+
             const subject = btn.getAttribute('data-subject'),
                 input = form.querySelector('[name="subject"]')
 
